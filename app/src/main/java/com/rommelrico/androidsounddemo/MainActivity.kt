7212120c676021dd4ad2948ffb8dc85a97fb63a3
Rommel Rico <me@rommelrico.com>
2018-10-31 19:42:03 -0700

    Creating the Play function

--- app/src/main/java/com/rommelrico/androidsounddemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidsounddemo/MainActivity.kt
@@ -3,14 +3,19 @@ package com.rommelrico.androidsounddemo
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mediaPlayer = MediaPlayer.create(this, R.raw.marbles)
+    }
 
-        val mediaPlayer = MediaPlayer.create(this, R.raw.marbles)
-        mediaPlayer.start()
+    fun play(view: View) {
+        mediaPlayer?.start()
     }
 }
