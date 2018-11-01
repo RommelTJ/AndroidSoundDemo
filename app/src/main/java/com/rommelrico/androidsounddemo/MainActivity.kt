ba9b4d3bd68dc375952e39a4b789084c1674883b
Rommel Rico <me@rommelrico.com>
2018-10-31 19:34:23 -0700

    Creating a MediaPlayer

--- app/src/main/java/com/rommelrico/androidsounddemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidsounddemo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.rommelrico.androidsounddemo
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mediaPlayer = MediaPlayer.create(this, R.raw.marbles)
     }
 }
