a15bf456091d3b26802c38f2cf8f987ef626591a
Rommel Rico <me@rommelrico.com>
2018-11-01 08:34:54 -0700

    Setting an Audio Manager and max volume

--- app/src/main/java/com/rommelrico/androidsounddemo/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidsounddemo/MainActivity.kt
@@ -1,33 +1,36 @@
 package com.rommelrico.androidsounddemo
 
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
 
     var mediaPlayer: MediaPlayer? = null
+    var audioManager: AudioManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mediaPlayer = MediaPlayer.create(this, R.raw.marbles)
 
+        // Max Volume
+        val maxVolume = audioManager?.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+
         // Handling the seekbar.
         val volumeControl = findViewById<SeekBar>(R.id.volumeSeekBar)
         volumeControl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
         })
 
